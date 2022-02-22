124604f731c8b7f56210f95accee5b0e2ed2ff13
felipemunevarn <felipemunevarn@gmail.com>
2022-02-22 10:54:17 -0300

    authentically encrypting password

--- src/main/java/com/sofka/api/api/ApiApplication.java
+++ src/main/java/com/sofka/api/api/ApiApplication.java
@@ -2,10 +2,10 @@ package com.sofka.api.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
-//@EnableEncryptableProperties
+@EnableEncryptableProperties
 public class ApiApplication {
 
 	public static void main(String[] args) {
